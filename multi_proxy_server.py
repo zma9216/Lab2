2baaac74ef68845d622ae391875855169a517b26
zma9216 <71898694+zma9216@users.noreply.github.com>
2021-09-17 13:23:31 -0600

    Completed multi proxy server

--- multi_proxy_server.py
+++ multi_proxy_server.py
@@ -1,24 +1,70 @@
 import socket, time, sys
 from multiprocessing import Process
 
-# TO-DO: get_remote_ip() method
+# Define global address and buffer size
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
 
-# TO-DO: handle_request() method
+
+# Get IP
+def get_remote_ip(host):
+    print(f'Getting IP for {host}')
+    try:
+        remote_ip = socket.gethostbyname(host)
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    print(f'Ip address of {host} is {remote_ip}')
+    return remote_ip
+
+
+def handle_request(addr, conn, end_proxy):
+    print("Connected by ", addr)
+
+    send_full_data = conn.recv(BUFFER_SIZE)
+    print(f"Sending received data {send_full_data} to Google")
+    end_proxy.sendall(send_full_data)
+    end_proxy.shutdown(socket.SHUT_WR)
+
+    data = end_proxy.recv(BUFFER_SIZE)
+    print(f"Sending received data {data} to client")
+    conn.send(data)
+
 
 def main():
-    # Establish localhost, extern_host (Google), port, and buffer size
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_start: # Establish "start" of proxy (connects to localhost)
-        # TO-DO: bind, and set to listening mode
+    # Establish extern_host (Google) and port
+    extern_host = "www.google.com"
+    extern_port = 80
+
+    with socket.socket(socket.AF_INET,
+                       socket.SOCK_STREAM) as proxy_start:  # Establish "start" of proxy (connects to localhost)
+        # Bind, and set to listening mode
+        proxy_start.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        proxy_start.bind((HOST, PORT))
+        proxy_start.listen(1)
 
         while True:
-            # TO-DO: Accept incoming connections from proxy_start and print information about connection
+            # Accept incoming connections from proxy_start and print information about connection
+            conn, addr = proxy_start.accept()
 
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_end: # Establish "end" of proxy (connects to Google)
-                # TO-DO: Get remote IP of Google and connect proxy_end to it
+            with socket.socket(socket.AF_INET,
+                               socket.SOCK_STREAM) as proxy_end:  # Establish "end" of proxy (connects to Google)
+                # Get remote IP of Google and connect proxy_end to it
+                print("Connecting to Google")
+                remote_ip = get_remote_ip(extern_host)
+                proxy_end.connect((remote_ip, extern_port))
 
-                # Multiprocessing...
+                # Allow for multiple connections with Process daemon
+                p = Process(target=handle_request, args=(addr, conn, proxy_end))  # Set target = handle_request
+                p.daemon = True
+                p.start()
+                print("Started process ", p)
 
-                # TO-DO: Allow for multiple connections with Process daemon
-                # Set target = handle_request
+            # Close the connection
+            conn.close()
 
-            # TO-DO: Close the connection
+
+if __name__ == "__main__":
+    main()
